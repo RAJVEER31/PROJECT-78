7cd648946d0ee8948e128f4cd7ec901787288a7e
RAJVEER31 <88730146+RAJVEER31@users.noreply.github.com>
2021-10-16 14:49:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg", "https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg", "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book", "Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var images = ["https://kbimages1-a.akamaihd.net/73c58c2c-c205-49ac-a745-86d25c8081ee/1200/1200/False/the-family-book.jpg", "file:///C:/Users/Admin/Pictures/Camera%20Roll/WIN_20210828_15_17_40_Pro%20(2).jpg", "file:///C:/Users/Admin/Pictures/Camera%20Roll/WhatsApp%20Image%202021-10-15%20at%2010.40.40%20PM%20(2).jpeg", "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Fmaily Book", "Me", "My Parents", "Rocky Singh", "Alia Singh", "Soni Singh"];
 var i = 0;
 function update() {
   i++;
