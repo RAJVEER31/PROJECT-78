f9de044e07a7ff6ae9bfaacd00c633db1c1e78f5
RAJVEER31 <88730146+RAJVEER31@users.noreply.github.com>
2021-10-16 15:48:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var images = ["https://kbimages1-a.akamaihd.net/73c58c2c-c205-49ac-a745-86d25c8081ee/1200/1200/False/the-family-book.jpg", "file:///C:/Users/Admin/Pictures/Camera%20Roll/WIN_20210828_15_17_40_Pro%20(2).jpg", "file:///C:/Users/Admin/Pictures/Camera%20Roll/WhatsApp%20Image%202021-10-15%20at%2010.40.40%20PM%20(2).jpeg", "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book", "Me", "My Parents", "Rocky Singh", "Alia Singh", "Soni Singh"];
+var images = ["https://kbimages1-a.akamaihd.net/73c58c2c-c205-49ac-a745-86d25c8081ee/1200/1200/False/the-family-book.jpg", "file:///C:/Users/Admin/Pictures/Camera%20Roll/WIN_20210828_15_17_40_Pro%20(2).jpg", "file:///C:/Users/Admin/Pictures/Camera%20Roll/WhatsApp%20Image%202021-10-15%20at%2010.40.40%20PM%20(2).jpeg", "file:///C:/Users/Admin/Pictures/Camera%20Roll/WhatsApp%20Image%202021-10-16%20at%2011.10.22%20AM%20(3).jpeg", "file:///C:/Users/Admin/Pictures/Camera%20Roll/WhatsApp%20Image%202021-10-16%20at%2011.12.51%20AM%20(2).jpeg"];
+var names = ["Fmaily Book", "Me", "My Parents", "My Grandfather", "My Brother"];
 var i = 0;
 function update() {
   i++;
-  var numbers_of_family_member_in_array = 5
+  var numbers_of_family_member_in_array = 4
   if (i > numbers_of_family_member_in_array) {
     i = 0;
   }
